235ed352b90a5c8ba0035d173e8472ede1539122
ICR07 <isramirezr@unal.edu.co>
2025-09-20 11:08:30 -0500

    v1

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -13,10 +13,10 @@ def test_01():
     digits = datasets.load_digits(return_X_y=True)
     data, target = digits
 
-    if not os.path.exists("homework/estimator.pkl"):
-        raise FileNotFoundError("homework/estimator.pkl not found")
+    if not os.path.exists("homework/estimator.pikle"):
+        raise FileNotFoundError("homework/estimator.pikle not found")
 
-    with open("homework/estimator.pkl", "rb") as file:
+    with open("homework/estimator.pikle", "rb") as file:
         new_clf = pickle.load(file)
 
     accuracy = accuracy_score(
